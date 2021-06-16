9b295d87d93f23f0467d72aae6d2bba33788aa99
raulisimo <raulraul.sp@gmail.com>
2021-06-16 20:12:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/sections/team-section.js
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Container, Grid } from 'theme-ui';
+import SectionHeader from 'components/section-header';
+import TeamCard from 'components/team-card';
+import { teamData } from "data/team";
+
+
+export default function TeamSection() {
+  return (
+    <section>
+      <Container>
+        <SectionHeader 
+        slogan="Our Team"
+        title="The most qualified and talented individuals"
+        />
+        <Grid sx={styles.grid}>
+          {teamData.map((item) => (
+            <TeamCard 
+            key={item.id}
+            src={item.imgSrc}
+            altText={item.altText}
+            title={item.title}
+            designation={item.designation}
+            social={item.socialProfile}
+
+            />
+
+          
+          ))}
+
+        </Grid>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  grid: {
+    mt: [0, null, -6, null, -4],
+    gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
+    gridTemplateColumns: [
+      'repeat(2,1fr)',
+      null,
+      'repeat(2,1fr)',
+      null,
+      'repeat(3,1fr)',
+    ],
+  },
+};
